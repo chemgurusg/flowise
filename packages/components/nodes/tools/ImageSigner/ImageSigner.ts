a536b3ef2fb8a6f6aabcbe61b13ab5ca00539c96
Maverick <akaras80@yahoo.com.sg>
2025-05-08 08:53:42 +0800

    Add ImageSigner tool package

---
+++ packages/components/nodes/tools/ImageSigner/ImageSigner.ts
@@ -0,0 +1,47 @@
+import { INode, INodeData, INodeParams } from '../../../interface';
+import crypto from 'crypto';
+
+class ImageSignerTool implements INode {
+    label = 'Image Signer Tool';
+    name = 'imageSignerTool';
+    type = 'Tool';
+    icon = 'globe.svg';
+    category = 'Utilities';
+    description = 'Signs image_id with token and expiry';
+
+    inputs: INodeParams[] = [
+        {
+            label: 'Image ID',
+            name: 'image_id',
+            type: 'string'
+        },
+        {
+            label: 'Secret Key',
+            name: 'secret_key',
+            type: 'string',
+            secret: true
+        },
+        {
+            label: 'Expiry (in seconds)',
+            name: 'expiry_seconds',
+            type: 'number',
+            default: 300
+        }
+    ];
+
+    async run(nodeData: INodeData): Promise<any> {
+        const image_id = nodeData.inputs?.image_id as string;
+        const secret_key = nodeData.inputs?.secret_key as string;
+        const expiry = Math.floor(Date.now() / 1000) + (nodeData.inputs?.expiry_seconds as number || 300);
+
+        const sig = crypto
+            .createHmac('sha256', secret_key)
+            .update(image_id + expiry)
+            .digest('hex');
+
+        const signed_url = `https://chemistryguru.com.sg/get-image.php?id=${image_id}&expires=${expiry}&sig=${sig}`;
+        return { signed_image_url: signed_url };
+    }
+}
+
+module.exports = { nodeClass: ImageSignerTool };
